2ff1caf9e402ba008c68ba9986f6c0c7c1fc07ed
TamamuraSyuuki <syuuki0105@gmail.com>
2018-09-18 20:38:56 +0900

    Transformクラスを更新

--- MyLibrary/Component/Transform.cpp
+++ MyLibrary/Component/Transform.cpp
@@ -2,4 +2,24 @@
 // Transform.cpp
 //
 
+#include "../pch.h"
 #include "Transform.h"
+
+using namespace MyLibrary;
+using namespace DirectX;
+using namespace DirectX::SimpleMath;
+
+/// <summary>
+/// コンストラクタ
+/// </summary>
+Transform::Transform()
+	: m_position(Vector3::Zero), m_rotation(Vector3::Zero), m_scale(0.f), m_world(Matrix::Identity)
+{
+}
+
+/// <summary>
+/// デストラクタ
+/// </summary>
+Transform::~Transform()
+{
+}
--- MyLibrary/Component/Transform.h
+++ MyLibrary/Component/Transform.h
@@ -3,13 +3,56 @@
 //
 #pragma once
 
+#include "Component.h"
+
 namespace MyLibrary
 {
 	/// <summary>
 	/// トランスフォームクラス
 	/// </summary>
-	class Transform
+	class Transform : public Component
 	{
+	public:
 
+		// コンストラクタ
+		Transform();
+
+		// デストラクタ
+		~Transform();
+
+		// ワールド行列の取得
+		DirectX::SimpleMath::Matrix GetWorld() { return m_world; }
+		// ワールド行列の設定
+		void SetWorld(DirectX::SimpleMath::Matrix world) { m_world = world; }
+
+		// 座標を取得
+		DirectX::SimpleMath::Vector3 GetPosition() { return m_position; }
+		// 座標を設定
+		void SetPosition(DirectX::SimpleMath::Vector3 position) { m_position = position; }
+		void SetPosition(float x, float y, float z) { m_position = DirectX::SimpleMath::Vector3(x, y, z); }
+
+		// スケールを取得
+		DirectX::SimpleMath::Vector3 GetScale() { return m_scale; }
+		// スケールを設定
+		void SetScale(DirectX::SimpleMath::Vector3 scale) { m_scale = scale; }
+		void SetScale(float x, float y, float z) { m_scale = DirectX::SimpleMath::Vector3(x, y, z); }
+
+		// 回転を取得
+		DirectX::SimpleMath::Quaternion GetRotation() { return m_rotation; }
+
+
+	private:
+
+		// 座標
+		DirectX::SimpleMath::Vector3 m_position;
+
+		// スケール
+		DirectX::SimpleMath::Vector3 m_scale;
+
+		// 回転
+		DirectX::SimpleMath::Quaternion m_rotation;
+
+		// ワールド行列
+		DirectX::SimpleMath::Matrix m_world;
 	};
 }

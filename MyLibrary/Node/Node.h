1d560d2891e6777bdf7c8bf0e956f35679dbcafc
TamamuraSyuuki <syuuki0105@gmail.com>
2018-09-18 20:18:37 +0900

    Nodeクラスを更新 .gitignoreにDebugを追加

--- MyLibrary/Node/Node.h
+++ MyLibrary/Node/Node.h
@@ -3,6 +3,8 @@
 //
 #pragma once
 
+#include <list>
+
 namespace MyLibrary
 {
 	/// <summary>
@@ -10,6 +12,36 @@ namespace MyLibrary
 	/// </summary>
 	class Node
 	{
+	public:
 
+		// コンストラクタ
+		Node();
+
+		// 仮想デストラクタ
+		virtual ~Node();
+
+		// 子を追加
+		void AddChild(Node* pNode);
+
+		// 全てを更新する
+		void UpdateAll(float elapsedTime);
+
+		// 全て描画する
+		void DrawAll();
+
+		
+	private:
+
+		// 自身の更新処理
+		virtual void Update(float elapsedTime);
+
+		// 自身の描画処理
+		virtual void Draw();
+
+
+	private:
+
+		// 子ノード
+		std::list<Node*> m_pChildren;
 	};
 }
